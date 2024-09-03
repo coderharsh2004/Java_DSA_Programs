faf63c89952c0e0831119b7a8ff45785c1b01dcf
coderharsh2004 <progamerharsh2004@gmail.com>
2024-09-03 19:48:18 +0530

    Linear Search

--- Arrays/src/sorting.java
+++ Arrays/src/sorting.java
@@ -9,18 +9,9 @@ public class sorting {
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        for (int i=0;i<(n-1);i++){
-            for(int j=0;j<(n-i-1);j++){
-                if(arr[j]>arr[j+1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
-            }
-        }
-        System.out.println("The sorted array is: ");
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
+        for(int i=0;i<n-1;i++){
+
         }
     }
+
 }
